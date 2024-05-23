198f92262c4d01c0bbf39a59c9658bc5f4441a18
puppykiwi <johnraymwendwa04@gmail.com>
2024-05-23 11:05:55 +0300

    first

--- app.py
+++ app.py
@@ -2,21 +2,38 @@ import os
 import spotipy
 import spotipy.util as util
 from dotenv import dotenv_values
+from spotipy.oauth2 import SpotifyOAuth
 
-ID = dotenv_values(".env")["ID"]
-SECRET = dotenv_values(".env")["SECRET"]
+
 
 # REDIRECT_URI = "https://mwendwatech.000webhostapp.com"
-REDIRECT_URI = "http://localhost:8888/callback"
-SCOPE = "user-library-read"
+# token = util.prompt_for_user_token( username = "puppykiwi",
+                                    # scope = SCOPE,
+                                    # client_id= ID,
+                                    # client_secret= SECRET,
+                                    # redirect_uri= REDIRECT_URI)
+# sp_oauth = SpotifyOAuth(client_id=ID,
+                        # client_secret=SECRET,
+                        # redirect_uri=REDIRECT_URI,
+                        # scope=SCOPE)
+# 
+# token_info = sp_oauth.get_access_token()
 
-token = util.prompt_for_user_token( username = "puppykiwi",
-                                    scope = SCOPE,
-                                    client_id= ID,
-                                    client_secret= SECRET,
-                                    redirect_uri= REDIRECT_URI)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID,
+                                               client_secret=SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope=SCOPE))
 
+# refresh_token = sp['refresh_token']
 
+results = sp.current_user_saved_tracks()
 
 if __name__ == "__main__":
-    print(ID, SECRET)
+    # print(sp['access_token'], "refresh token {}",refresh_token)
+    # print(results)
+    
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        print(idx, track['artists'][0]['name'], " – ", track['name'])
+    print("** DONE **")
+    
